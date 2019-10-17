ab860ff2fc69c80ecad07d54f7e9165d0e79d182
chenjiahao12 <chen1352192872@163.com>
2019-10-17 14:20:05 +0800

    git send local update 1

--- gitssm/src/com/cjh/git/Demo.java
+++ gitssm/src/com/cjh/git/Demo.java
@@ -8,6 +8,6 @@ package com.cjh.git;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("git idea test update");
+        System.out.println("git idea test update 1 ");
     }
 }
