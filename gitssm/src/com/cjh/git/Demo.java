7bcf8e908e162349e98d9513162d77d75a19e839
chenjiahao12 <chen1352192872@163.com>
2019-10-17 14:15:14 +0800

    git send local

---
+++ gitssm/src/com/cjh/git/Demo.java
@@ -0,0 +1,13 @@
+package com.cjh.git;
+
+/**
+ * @author
+ * @site
+ * @company
+ * @create 2019-10-17 14:11
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("git idea test");
+    }
+}
